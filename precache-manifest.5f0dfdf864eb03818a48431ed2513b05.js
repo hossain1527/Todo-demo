85e871dce8ddb1280f674ee31bf60cee299b5153
hossain1527 <hossain.sci2015@gmail.com>
2020-10-14 22:51:54 +0100

    Updates

---
+++ precache-manifest.5f0dfdf864eb03818a48431ed2513b05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5353e5bf6204bcdbfb25bfe6a03e270c",
+    "url": "/Todo-demo/index.html"
+  },
+  {
+    "revision": "99b0ef05f0c2407eb449",
+    "url": "/Todo-demo/static/css/main.bd704cbd.chunk.css"
+  },
+  {
+    "revision": "8949b2bb52a7336db61c",
+    "url": "/Todo-demo/static/js/2.4c153ab9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-demo/static/js/2.4c153ab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99b0ef05f0c2407eb449",
+    "url": "/Todo-demo/static/js/main.6ad4c613.chunk.js"
+  },
+  {
+    "revision": "f46e1d951dc12756a3f6",
+    "url": "/Todo-demo/static/js/runtime-main.56320d89.js"
+  }
+]);
